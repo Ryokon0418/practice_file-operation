7e358b863aebaee5ade09dad1b288d0d8aacd543
Ryokon0418 <ryokon0418@gmail.com>
2020-11-14 13:52:24 +0900

    [update] 複数行分CSVファイルに出力する処理を追加

--- tools/operations/csv_operations/write/write_csv.py
+++ tools/operations/csv_operations/write/write_csv.py
@@ -1,5 +1,14 @@
 import csv
 
+# 1行分出力
+# with open('file.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(['TEST1', 'TEST2', 'TEST3'])
+
+# 複数行出力
+rows = []
 with open('file.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerow(['TEST1', 'TEST2', 'TEST3'])
+    rows = [['ミッフィー', 'ディズニー', '鬼滅'], [
+        'ミッフィー', 'ミッキー', 'たんじろう'], ['スナッフィー', 'ミニー', 'ねづこ'], ['ダーン', 'ドナルド', 'いのすけ']]
+    writer.writerows(rows)
